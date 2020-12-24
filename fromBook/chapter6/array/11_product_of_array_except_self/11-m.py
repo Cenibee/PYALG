900ae405c2cfe4b276be422ddd0821d23fb0f40a
unknown <ksj3452@naver.com>
2020-12-24 22:10:13 +0900

    배열 부분까지 했음

--- fromBook/chapter6/array/11_product_of_array_except_self/11-m.py
+++ fromBook/chapter6/array/11_product_of_array_except_self/11-m.py
@@ -7,3 +7,18 @@ class Solution:
         길이가 n > 1을 만족하는 배열 nums에 대해, output[i] 가 nums에서 num[i]를 제외한 모든 수의 곱인 output을 구하시오
         나누기를 사용하지 않고 O(n) 시간복잡도를 가지도록 구현하세요
         '''
+        left, right = 1, 1
+
+        size = len(nums)
+        output = [1] * size
+
+        for i in range(size - 1):
+            left *= nums[i]
+            right *= nums[~i]
+
+            output[i + 1] *= left
+            output[~i - 1] *= right
+        return output
+
+sol = Solution()
+print(sol.productExceptSelf([1,2,3,4]))
