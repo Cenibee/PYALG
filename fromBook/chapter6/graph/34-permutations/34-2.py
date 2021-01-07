47d182506fa5ee0682448a0690c0ff9bab7acfb2
unknown <ksj3452@naver.com>
2021-01-08 01:56:44 +0900

    그래프 - 순열과 조합 중

---
+++ fromBook/chapter6/graph/34-permutations/34-2.py
@@ -0,0 +1,6 @@
+import itertools
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
---
+++ fromBook/chapter6/graph/35-combinations/36-1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        results = []
+
+        def dfs(elements, start: int, k: int):
+            if k == 0:
+                results.append(elements[:])
+
+            for i in range(start, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1, k - 1)
+                elements.pop()
+
+        dfs([], 1, k)
+        return results
+
+
+sol = Solution()
+print(sol.combine(4, 2))
+a = []
