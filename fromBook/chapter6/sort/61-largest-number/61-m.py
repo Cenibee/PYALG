28818c146bcff3b57145d738cfebb2b92c1a7436
unknown <ksj3452@naver.com>
2021-01-25 00:42:22 +0900

    61 번 문제

--- fromBook/chapter6/sort/61-largest-number/61-m.py
+++ fromBook/chapter6/sort/61-largest-number/61-m.py
@@ -1,14 +1,18 @@
 from typing import List
-
+from functools import cmp_to_key
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        def template(num: str) -> str:
-            return num + (num[-1] * (10 - len(num)))
+        def template(str1: str, str2: str) -> int:
+            if str1 + str2 >= str2 + str1:
+                return -1
+            else:
+                return 1
 
         nums = [str(x) for x in nums]
-        nums.sort(reverse=True, key=template)
-        return ''.join(nums)
+        nums.sort(key=cmp_to_key(template))
+        if nums[0][0] != '0': return ''.join(nums)
+        else: return '0'
 
 sol = Solution()
 print(sol.largestNumber([34, 30, 9, 5, 3]))
@@ -20,3 +24,5 @@ def template(num: str) -> str:
 
 print(template(str(34323)))
 print(template(str(3432)))
+
+## 모르겠다 ㅈㅈ ㅠㅠㅠ
