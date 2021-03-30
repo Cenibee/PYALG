1b3da85b07a50752fcc527dcd39e28552337bba1
cenibee <ksj3452@naver.com>
2021-03-30 17:16:13 +0900

    점프와 순간 이동 문제 풀이

---
+++ java/src/main/java/programmers/level2/JumpAndTeleport.java
@@ -0,0 +1,10 @@
+package programmers.level2;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12980
+ */
+public class JumpAndTeleport {
+    public int solution(int n) {
+        return Integer.bitCount(n);
+    }
+}
