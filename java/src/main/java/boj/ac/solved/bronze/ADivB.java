91cd01a7afa9c181c45be63d814474a9566cf44a
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-06-15 21:26:29 +0900

    백준: A / B

---
+++ java/src/main/java/boj/ac/solved/bronze/ADivB.java
@@ -0,0 +1,12 @@
+package boj.ac.solved.bronze;
+
+import java.util.Scanner;
+
+public class ADivB {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(scn.nextDouble() / scn.nextDouble());
+    }
+
+}
