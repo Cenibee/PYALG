ce0798eeb7f8f53562a0ba007b82cd371cd1994e
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-06-15 21:40:05 +0900

    백준: 별 찍기 - 1

---
+++ java/src/main/java/boj/ac/solved/bronze/PrintStart1.java
@@ -0,0 +1,15 @@
+package boj.ac.solved.bronze;
+
+import java.util.Scanner;
+
+public class PrintStart1 {
+
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        StringBuilder star = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            System.out.println(star.append("*"));
+        }
+    }
+
+}
