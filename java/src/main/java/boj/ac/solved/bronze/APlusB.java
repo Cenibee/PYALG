eca0670e9b964e0cbe13d05cc68207b3270d65e7
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-06-15 19:51:43 +0900

    백준: A + B

---
+++ java/src/main/java/boj/ac/solved/bronze/APlusB.java
@@ -0,0 +1,12 @@
+package boj.ac.solved.bronze;
+
+import java.util.Scanner;
+
+public class APlusB {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(scn.nextInt() + scn.nextInt());
+    }
+
+}
