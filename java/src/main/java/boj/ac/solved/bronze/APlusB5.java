ecc5920a97bced9b74508486a8d703ea84981b43
cenibee <ksj3452@naver.com>
2021-06-17 16:23:13 +0900

    백준: A+B_5

---
+++ java/src/main/java/boj/ac/solved/bronze/APlusB5.java
@@ -0,0 +1,19 @@
+package boj.ac.solved.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class APlusB5 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        reader.lines().forEach(s -> {
+            if (s.startsWith("0")) return;
+            String[] split = s.split(" ");
+            System.out.println(Integer.parseInt(split[0]) + Integer.parseInt(split[1]));
+        });
+    }
+
+}
