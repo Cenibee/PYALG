18fa08c042ab38669986aecaff78967fce9c5496
cenibee <ksj3452@naver.com>
2021-06-17 16:08:49 +0900

    백준: 아스키코드

---
+++ java/src/main/java/boj/ac/solved/bronze/AsciiCode.java
@@ -0,0 +1,11 @@
+package boj.ac.solved.bronze;
+
+import java.io.IOException;
+
+public class AsciiCode {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.in.read());
+    }
+
+}
